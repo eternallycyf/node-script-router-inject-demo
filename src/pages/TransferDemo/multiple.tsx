5a78e2d1608835c0648b129782911c2d0b742be8
eternallycyf <a969475322@gmail.com>
2022-07-24 18:45:43 +0800

    feat: add

---
+++ src/pages/TransferDemo/multiple.tsx
@@ -0,0 +1,88 @@
+import TransferForm from "@/components/TransferForm";
+import styles from "./index.less";
+import React, { useRef, useState, useEffect } from "react";
+import { Form, Button } from "antd";
+
+const TransferDemo = () => {
+  // const TransferRef = useRef(null)
+  const [form] = Form.useForm();
+  const [mockData, setMockData] = useState<any>([]);
+  const TransferRef = useRef([]);
+
+  useEffect(() => {
+    const initData = Array.from({
+      length: 20,
+    }).map((_, i) => ({
+      key: i.toString(),
+      title: `content${i + 1}`,
+      description: `description of content${i + 1}`,
+    }));
+    setMockData(initData);
+  }, []);
+
+  const testData = () => {
+    const initData = Array.from({
+      length: 10,
+    }).map((_, i) => ({
+      key: i.toString(),
+      title: `content${i + 1}`,
+      description: `description of content${i + 1}`,
+    }));
+    setMockData(initData);
+  };
+
+  const transferOkCallBack = () => {
+    const { getData } = TransferRef.current[0];
+    console.log(form?.getFieldValue("test"));
+  };
+
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          TransferRef.current[0].useVisible(true);
+        }}
+      >
+        打开0
+      </button>
+      <button
+        onClick={() => {
+          TransferRef.current[1].useVisible(true);
+        }}
+      >
+        打开1
+      </button>
+      <button onClick={() => testData()}>set</button>
+      <Form form={form} onFinish={onFinish}>
+        ssssss
+        <Form.Item style={{ display: "inline-block" }}>
+          <TransferForm
+            formRef={form}
+            originData={mockData}
+            ref={(el) => (TransferRef.current[0] = el)}
+            transferOkCallBack={() => transferOkCallBack()}
+          />
+        </Form.Item>
+        <Form.Item style={{ display: "inline-block" }}>
+          <TransferForm
+            formRef={form}
+            originData={mockData}
+            ref={(el) => (TransferRef.current[1] = el)}
+            transferOkCallBack={() => transferOkCallBack()}
+            falseSelectItmeName="asasd"
+          />
+        </Form.Item>
+        sssss
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default TransferDemo;
